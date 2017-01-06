529b2650b5e00b485967c67423a151fdce9bf03d
Alex Radu <dragosthealx@gmail.com>
2017-01-06 17:40:47 +0200

    Added social icons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,6 +3,38 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12">
+          <ul class="social-icons">
+            <li>
+              <a class="social-link github">
+                <span style="background-image:url('{{asset('img/icons/github.svg')}}')">
+                </span>
+              </a>
+            </li>
+            <li>
+              <a class="social-link linkedin">
+                <span style="background-image:url('{{asset('img/icons/linkedin.svg')}}')">
+                </span>
+              </a>
+            </li>
+            <li>
+              <a class="social-link facebook">
+                <span style="background-image:url('{{asset('img/icons/facebook.svg')}}')">
+                </span>
+              </a>
+            </li>
+            <li>
+              <a class="social-link instagram">
+                <span style="background-image:url('{{asset('img/icons/instagram.svg')}}')">
+                </span>
+              </a>
+            </li>
+            <li>
+              <a class="social-link gplus">
+                <span style="background-image:url('{{asset('img/icons/gplus.svg')}}')">
+                </span>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
