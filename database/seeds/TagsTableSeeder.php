e607f521e78a7cca235055f72121c99ad2c32893
Alex Radu <dragosthealx@gmail.com>
2017-01-06 16:11:56 +0200

    Added a seeder for tags

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Tag::create([
+          'name'  => 'PHP'
+        ]);
+        Tag::create([
+          'name'  => 'Python'
+        ]);
+        Tag::create([
+          'name'  => 'Java'
+        ]);
+        Tag::create([
+          'name'  => 'CSS'
+        ]);
+        Tag::create([
+          'name'  => 'C#'
+        ]);
+        Tag::create([
+          'name'  => 'C'
+        ]);
+        Tag::create([
+          'name'  => 'C++'
+        ]);
+        Tag::create([
+          'name'  => 'Laravel'
+        ]);
+        Tag::create([
+          'name'  => 'Unity3D'
+        ]);
+        Tag::create([
+          'name'  => 'Linux'
+        ]);
+        Tag::create([
+          'name'  => 'JavaScript'
+        ]);
+        Tag::create([
+          'name'  => 'HTML'
+        ]);
+        Tag::create([
+          'name'  => 'Machine Learning'
+        ]);
+        Tag::create([
+          'name'  => 'Neural Networks'
+        ]);
+        Tag::create([
+          'name'  => 'Android'
+        ]);
+        Tag::create([
+          'name'  => 'Game'
+        ]);
+        Tag::create([
+          'name'  => 'Winner'
+        ]);
+    }
+}
