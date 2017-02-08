7e39c1ddfc1a79a63444c923caccda8459bf7956
Alex Radu <dragosthealx@gmail.com>
2017-02-08 06:14:08 +0000

    added description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="I am a passionate and enthusiast Artificial Intelligence student, always keen to learn new technologies. With enough determination, everything is possible.">
         <title>@yield('title')</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600"
