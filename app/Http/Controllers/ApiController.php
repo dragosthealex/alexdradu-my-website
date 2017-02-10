13dbc50728698bfeab548c88f3f5e77bd3085c77
Alex Radu <dragosthealx@gmail.com>
2017-02-10 11:36:35 +0000

    Fixed bug with github key

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,10 +9,14 @@ class ApiController extends Controller
   public function githubHook(Request $request) {
     $signature = $request->header('X-Hub-Signature');
     $payload = $request->input('payload');
-    if(hash_hmac('sha1', $payload, env('GIT_WEBHOOK_KEY', 'key')) != $signature) {
+    $pass = hash_hmac('sha1', $payload, env('GIT_WEBHOOK_KEY', 'key'));
+    if($pass != $signature) {
       echo "cacat";
     }
+    echo $pass;
     echo $signature;
+
+
     echo $payload;
   }
 
